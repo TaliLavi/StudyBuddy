5f03f182f08cb1ab8e4b9c3ed343d700917e3880
Tali Lavi <tali@lavi.fm>
2015-05-29 00:25:30 +0100

    Added jquery and the scrolable library. Created basic drag and drop functionality.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-/**
- * Created by talib on 28/05/2015.
- */
+
+//Make these things happen each time the page finishes loading
+function preparePage() {
+    //set up drag and drop for each list
+    $(".list-of-cards").each(function(i, list){
+        Sortable.create(list, {group: "stuff", onEnd: sayHi});
+    })
+}
+
+function sayHi() {
+    console.log("Hi!")
+}
