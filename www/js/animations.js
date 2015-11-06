102fee6e9a7c808cdefd574b4bff313b8f388566
RoisinOk <roisin_ok@hotmail.com>
2015-11-06 10:53:19 +0000

    Changed animation to scaleX and scaleY rather than height and width. Looks terrible on browser, but checking on iPad.  Commented out Ruzo to try on iPad.

--- www/js/animations.js
+++ www/js/animations.js
@@ -6,23 +6,23 @@ function animateRuzoDone() {
 
 
 
-    ruzoDoneTimeline.to("#eyesOpenMouthClosedDone", 1, {
-        rotation    :   "5",
-    }) ;
+    //ruzoDoneTimeline.to("#eyesOpenMouthClosedDone", 1, {
+    //    rotation    :   "5",
+    //}) ;
 
     ruzoDoneTimeline.to("#doneCircle", 2, {
-        height          :   400,
-        width           :   400,
+        scaleX          :   20,
+        scaleY          :   20,
         ease            :   Back.easeOut.config(2), y: 0
-
     });
-    ruzoDoneTimeline.to("#tailDoneRuzo2", 2, {
-        bottom          :   "-22px",
-        right           :   "-13px",
-        rotation        :   "90",
-        transformOrigin :   "80% 80%",
-        ease            :   Back.easeOut.config(1), y: 0
-    }, "-=1.75") ;
+
+    //ruzoDoneTimeline.to("#tailDoneRuzo2", 2, {
+    //    bottom          :   "-22px",
+    //    right           :   "-13px",
+    //    rotation        :   "90",
+    //    transformOrigin :   "80% 80%",
+    //    ease            :   Back.easeOut.config(1), y: 0
+    //}, "-=1.75") ;
 
     console.log("ruzoDoneTimeline should be prepared");
     ruzoDoneTimeline.play();
