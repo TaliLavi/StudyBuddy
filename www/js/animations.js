dccc63fb3ac91320df25d4fa1703ddefb66061bc
RoisinOk <roisin_ok@hotmail.com>
2015-11-06 10:33:57 +0000

    Added new file animations.js (to hold the code for ruzo animations during development). Added pngs with ruzo to git.

---
+++ www/js/animations.js
@@ -0,0 +1,29 @@
+var ruzoDoneTimeline = new TimelineMax({paused:true});
+
+
+
+function animateRuzoDone() {
+
+
+
+    ruzoDoneTimeline.to("#eyesOpenMouthClosedDone", 1, {
+        rotation    :   "5",
+    }) ;
+
+    ruzoDoneTimeline.to("#doneCircle", 2, {
+        height          :   400,
+        width           :   400,
+        ease            :   Back.easeOut.config(2), y: 0
+
+    });
+    ruzoDoneTimeline.to("#tailDoneRuzo2", 2, {
+        bottom          :   "-22px",
+        right           :   "-13px",
+        rotation        :   "90",
+        transformOrigin :   "80% 80%",
+        ease            :   Back.easeOut.config(1), y: 0
+    }, "-=1.75") ;
+
+    console.log("ruzoDoneTimeline should be prepared");
+    ruzoDoneTimeline.play();
+}
