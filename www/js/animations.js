7a6eda744a7db1604e698fc023276696f781b487
RoisinOk <roisin_ok@hotmail.com>
2015-11-06 11:15:27 +0000

    Changed animation back to height and width. But changed Ruzo tail to x and y.

--- www/js/animations.js
+++ www/js/animations.js
@@ -6,24 +6,29 @@ function animateRuzoDone() {
 
 
 
-    //ruzoDoneTimeline.to("#eyesOpenMouthClosedDone", 1, {
-    //    rotation    :   "5",
-    //}) ;
+    ruzoDoneTimeline.to("#eyesOpenMouthClosedDone", 1, {
+        rotation    :   "0",
+    }) ;
 
     ruzoDoneTimeline.to("#doneCircle", 2, {
-        scaleX          :   20,
-        scaleY          :   20,
+        height          :   400,
+        width           :   400,
         ease            :   Back.easeOut.config(2), y: 0
+
     });
 
-    //ruzoDoneTimeline.to("#tailDoneRuzo2", 2, {
-    //    bottom          :   "-22px",
-    //    right           :   "-13px",
-    //    rotation        :   "90",
-    //    transformOrigin :   "80% 80%",
-    //    ease            :   Back.easeOut.config(1), y: 0
-    //}, "-=1.75") ;
+    ruzoDoneTimeline.to("#tailDoneRuzo2", 2, {
+        x               :   "22",
+        y               :   "-13",
+        rotation        :   "90",
+        transformOrigin :   "80% 80%",
+        ease            :   Back.easeOut.config(1), y: 0
+    }, "-=1.75") ;
 
     console.log("ruzoDoneTimeline should be prepared");
     ruzoDoneTimeline.play();
+}
+
+function restartRuzoDone(){
+    ruzoDoneTimeline.restart();
 }
